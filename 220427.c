bbe2a42f978a46f467f38cb8b515b14cff4dac2d
Laptop <Laptop@DESKTOP-K3BTJTM>
2022-05-31 19:30:43 +0800

    添加项目文件。

---
+++ 220427.c
@@ -0,0 +1,32 @@
+//#include<stdio.h>
+//
+//int main()                初识打印 
+//{
+//	printf("HELLO WORLD\n");
+//	return 0;
+//}
+
+
+
+
+
+
+#include<stdio.h>
+
+int main()  //打印飞机 
+{
+    printf("\n\
+    @\n\
+    /\\\n\
+    **\n\
+    **\n\
+    **\n\
+  ******\n\
+ ********\n\
+**********\n\
+    **\n\
+    **\n\
+   ****\n\
+  ******\n");
+    return 0;
+}
